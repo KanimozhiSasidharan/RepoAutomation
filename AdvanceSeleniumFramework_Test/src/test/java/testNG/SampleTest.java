62e31bd06ed2d77af5dfe6a3c2d6bb855e475c67
Kanimozhi <Kanimozhi@DESKTOP-H8NBO3R>
2025-03-08 15:32:13 +0530

    push 1

---
+++ AdvanceSeleniumFramework_Test/src/test/java/testNG/SampleTest.java
@@ -0,0 +1,25 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class SampleTest 
+{
+	@Test(priority = 2,invocationCount=2)
+	public void createProduct()
+	{
+		System.out.println("Product created");
+	}
+	
+	@Test(dependsOnMethods = "createProduct",priority = 1)
+	public void modifyProduct()
+	{
+		System.out.println("Product modified");
+	}
+	
+	@Test(dependsOnMethods = "createProduct",priority = 0)
+	public void deleteProduct()
+	{
+		System.out.println("Product Deleted");
+	}
+
+}
